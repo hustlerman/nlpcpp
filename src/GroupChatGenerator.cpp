7e55ca2194875378a5e4e4c138aa5f899b8d1e56
Chris Benfante <cabenfan@ncsu.edu>
2018-12-17 20:01:41 -0500

    Working generator, bug with flipped ngrams mapping?

--- src/GroupChatGenerator.cpp
+++ src/GroupChatGenerator.cpp
@@ -8,24 +8,24 @@
 int main() {
     std::map<std::string, MarkovChain*> users;
     std::vector<std::string> names;
-    users.insert(std::pair<std::string, MarkovChain*>("Sierra LaPlante", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Kristen Lovell", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Brenden Micciche", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Sam Rappl", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Angela Hoyt", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Megan Hood", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Katie Hazel", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Jeremy Kelly", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Kayla Oh", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Chris B", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Chloe Campbell", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Ryan Gramazy", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Emi G", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Nick Latz", new MarkovChain(3)));
-    users.insert(std::pair<std::string, MarkovChain*>("Matt Wyant", new MarkovChain(3)));
+    users.insert(std::pair<std::string, MarkovChain*>("Sierra LaPlante", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Kristen Lovell", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Brendan Micciche", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Sam Rappl", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Angela Hoyt", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Megan Hood", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Katie Hazel", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Jeremy Kelly", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Kayla Oh", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Chris B", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Chloe Campbell", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Ryan Gramzay", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Emi G", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Nick Latz", new MarkovChain(1)));
+    users.insert(std::pair<std::string, MarkovChain*>("Matt Wyant", new MarkovChain(1)));
     names.push_back("Sierra LaPlante");
     names.push_back("Kristen Lovell");
-    names.push_back("Brenden Micciche");
+    names.push_back("Brendan Micciche");
     names.push_back("Sam Rappl");
     names.push_back("Angela Hoyt");
     names.push_back("Megan Hood");
@@ -34,31 +34,26 @@ int main() {
     names.push_back("Kayla Oh");
     names.push_back("Chris B");
     names.push_back("Chloe Campbell");
-    names.push_back("Ryan Gramazy");
+    names.push_back("Ryan Gramzay");
     names.push_back("Emi G");
     names.push_back("Nick Latz");
     names.push_back("Matt Wyant");
     std::ifstream groupchat("groupchat.txt");
     std::string line;
     std::getline(groupchat, line);
-
-    std::cout << "Grabbed first line! " << std::endl;
-    std::cout << line << std::endl; 
-
+    std::fstream message;
     //Load training data from all of the users
     while(users.find(line) != users.end()) {
         MarkovChain* user = users.at(line);
-        std::fstream message("temp.txt", std::fstream::in | std::fstream::out);
+        std::cout << "Reading text for " << line << std::endl;
+	    message.open("temp.txt", std::fstream::in | std::fstream::out | std::fstream::trunc);
         while(std::getline(groupchat, line) && !line.empty() && users.find(line) == users.end()) {
-            std::cout << "Here I am!" << std::endl;
             message << line << std::endl;
+            std::cout << line << std::endl;
         }
-        std::cout << "Now I'm Here!" << std::endl;
-        message.seekg(0, std::fstream::beg);
-        std::cout << message.tellg() << std::endl;
+        message.seekg(0, std::ios::beg);
         user->readText(message);
         message.close();
-        remove("temp.txt");
     }
 
     for(std::map<std::string, MarkovChain*>::iterator it = users.begin(); it != users.end(); ++it) {
@@ -70,8 +65,10 @@ int main() {
     for(int i = 0; i < 100; i++) {
         std::string name = names.at(rand()%names.size());
         MarkovChain* currUser = users.at(name);
-        generatedchat << name << ": "<< std::endl << currUser->generateText() << std::endl;
+        currUser->printNGrams();
+        std::cout << "\n" << std::endl;
+        std::cout << "Beginning to generate text for " << name << "!" << std::endl;
+        generatedchat << name << ": \n" << currUser->generateText() << std::endl;
     }
-
     return 0;
 }
--- src/MarkovChain.cpp
+++ src/MarkovChain.cpp
@@ -7,14 +7,18 @@
 
 class MarkovChain {
     private:
-    std::map<std::string, std::vector<std::string*>*>* ngrams;
+    std::map<std::string*, std::vector<std::string*>*>* ngrams;
+    std::vector<std::string*>* startingNgrams;
     std::map<std::string, std::string*>* dictionary;
+    std::map<std::string, std::string*>* ngramDictionary;
     int order;
 
     public:
     MarkovChain(int order) {
-        ngrams = new std::map<std::string, std::vector<std::string*>*>();
+        ngrams = new std::map<std::string*, std::vector<std::string*>*>();
+        startingNgrams = new std::vector<std::string*>();
         dictionary = new std::map<std::string, std::string*>();
+        ngramDictionary = new std::map<std::string, std::string*>();
         this->order = order;
     }
 
@@ -22,6 +26,8 @@ class MarkovChain {
         std::string temp;
         std::deque<std::string> currentNgram;
         std::string currentNgramString;
+        std::string* word;
+        std::string* ngram;
         //Initialize the first Ngram and beginning of the dictionary
         for(int i = 0; i < order && corpus >> temp; i++) {
             currentNgram.push_back(temp);
@@ -29,17 +35,26 @@ class MarkovChain {
                 dictionary->insert(std::pair<std::string, std::string*>(temp, new std::string(temp)));
             }
         }
-        if(currentNgram.size() != order) {
+        if(currentNgram.size() != this->order) {
             return;
         }
+        //Create first ngram and store it in starting Ngrams
+        currentNgramString = currentNgram.front();
+        for(int i = 1; i < currentNgram.size(); i++) {
+            currentNgramString += " " + currentNgram.at(i);
+        }
+        ngram = new std::string(currentNgramString);
+        startingNgrams->push_back(ngram);
+        
         while(corpus >> temp) {
-            //Construct a string from the ngram deque
-            currentNgramString = currentNgram.at(0);
-            for(int i = 1; i < currentNgram.size(); i++) {
-                currentNgramString += " " + currentNgram.at(i);
+            //Check to see if the ngram exists in the ngramDictionary
+            if(ngramDictionary->find(currentNgramString) == ngramDictionary->end()) {
+                ngram = new std::string(currentNgramString);
+                ngramDictionary->insert(std::pair<std::string, std::string*>(currentNgramString, ngram));
+            } else {
+                ngram = ngramDictionary->at(currentNgramString);
             }
             //Check to see if the next word is in the dictionary and get its pointer
-            std::string* word;
             if(dictionary->find(temp) == dictionary->end()) {
                 word = new std::string(temp);
                 dictionary->insert(std::pair<std::string, std::string*>(temp, word));
@@ -49,23 +64,28 @@ class MarkovChain {
             //Check to see if the current ngram already exists,
             //create if it doesn't, otherwise, append it to current list
             std::vector<std::string*>* follow;
-            if(ngrams->find(currentNgramString) == ngrams->end()) {
+            if(ngrams->find(ngram) == ngrams->end()) {
                 follow = new std::vector<std::string*>();
-                ngrams->insert(std::pair<std::string, std::vector<std::string*>*>(currentNgramString, follow));
+                ngrams->insert(std::pair<std::string*, std::vector<std::string*>*>(ngram, follow));
             } else {
-                std::vector<std::string*>* follow = ngrams->at(currentNgramString);
+                std::vector<std::string*>* follow = ngrams->at(ngram);
             }
             follow->push_back(word);
             currentNgram.pop_front();
             currentNgram.push_back(temp);
+            //Construct the next string from the ngram deque
+            currentNgramString = currentNgram.front();
+            for(int i = 1; i < currentNgram.size(); i++) {
+                currentNgramString += " " + currentNgram.at(i);
+            }
         }
     }
 
     void printNGrams() {
-        std::map<std::string, std::vector<std::string*>*>::iterator it;
+        std::map<std::string*, std::vector<std::string*>*>::iterator it;
         std::vector<std::string*>::iterator jt;
         for(it = ngrams->begin(); it != ngrams->end(); ++it) {
-            std::cout << it->first << ':';
+            std::cout << *it->first << ':';
             jt = it->second->begin();
             std::cout << "[" << **jt;
             ++jt;
@@ -77,19 +97,38 @@ class MarkovChain {
     }
 
     std::string generateText() {
-        std::map<std::string, std::vector<std::string*>*>::iterator it;
-        if(ngrams->size() == 0) {
-            return "";
-        }
-        std::advance( it, rand()%ngrams->size() );
-        std::string currentNgram = it->first;
+        //Randomly select a starting ngram
+        std::string currentNgram = *startingNgrams->at(rand()%startingNgrams->size());
+        std::string tempNgram = currentNgram;
+        std::string *ngramPointer;
+        //Split starting ngram into the result vector on spaces
         std::vector<std::string*> result;
+        bool split = true;
+        while(split) {
+            int index = tempNgram.find(' ');
+            if(index != -1) {
+                std::cout << tempNgram << std::endl;
+                result.push_back(dictionary->at(tempNgram.substr(0, index)));
+                tempNgram = tempNgram.substr(index+1, tempNgram.length());
+                std::cout << tempNgram << std::endl;
+            } else {
+                split = false;
+            }
+        }
+        std::cout << tempNgram << std::endl;
+        std::cout << "Loading first ngram..." << std::endl;
+        result.push_back(dictionary->at(tempNgram));
+        std::cout << "Loaded first ngram!" << std::endl; 
         std::string resultString = currentNgram;
-        for(int i = 0; i < 30; i++) {
-            if (ngrams->find(currentNgram) == ngrams->end()) {
+        for(int i = 0; i < 100; i++) {
+            if(ngramDictionary->find(currentNgram) == ngramDictionary->end()) {
                 break;
             }
-            std::vector<std::string*>* possibilities = ngrams->at(currentNgram);
+            ngramPointer = ngramDictionary->at(currentNgram);
+            if(ngrams->find(ngramPointer) == ngrams->end()) {
+                break;
+            }
+            std::vector<std::string*>* possibilities = ngrams->at(ngramPointer);
             if(possibilities->size() == 0) {
                 break;
             }
@@ -98,8 +137,10 @@ class MarkovChain {
             resultString += " " + *nextWord;
             currentNgram = "";
             for(int j = result.size()-order; j < result.size(); j++) {
-                currentNgram += *result.at(j);
+                currentNgram += *result.at(j) + " ";
             }
+            //Remove the extra space
+            currentNgram = currentNgram.substr(0, currentNgram.size() - 1);
         }
         return resultString;
     }
@@ -110,5 +151,6 @@ class MarkovChain {
 //     std::ifstream corpus("beemovie.txt");
 //     test.readText(corpus);
 //     test.printNGrams();
+//     std::cout << test.generateText() << std::endl;
 //     return 0;
 // }
